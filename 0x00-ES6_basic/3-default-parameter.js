6df0eb899adc565d1d7a813db320484a5147e097
dannykay62 <danielkay62@gmail.com>
2023-09-29 15:20:21 +0100

    Condense the internals of the following function to 1 line - without changing the name of each function/variable.
    Hint: The key here to define default parameter values for the function parameters.

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1989 = 89, enpansion2019 = 19) {
-	eturn initialNumber + expansion1989 + expansion2019;
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+	  return initialNumber + expansion1989 + expansion2019;
 }
