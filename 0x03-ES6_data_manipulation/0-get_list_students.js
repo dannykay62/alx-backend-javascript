8bc2026667413371a23fc6841c6515a0e751a0f6
dannykay62 <danielkay62@gmail.com>
2023-10-03 23:47:28 +0100

    a function named getListStudents that returns an array of objects.
    Each object should have three attributes: id (Number), firstName (String), and location (String).

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+const getListStudents = () => [
+	{ id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+	{ id: 2, firstname: 'James', location: 'Columbia' },
+	{ id: 5, firstname: 'Serena', location: 'San Francisco' },
+];
+
+export default getListStudents;
