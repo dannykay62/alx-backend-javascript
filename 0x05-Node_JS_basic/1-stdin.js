94c06a7f8dc98604cd7878c45340efc2a65190f7
dannykay62 <danielkay62@gmail.com>
2023-11-25 00:19:47 +0100

    Using Process stdin Create a program  that will be executed through command line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,6 +4,7 @@ process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) process.stdout.write(`Your name is: ${name}`);
 });
+
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
