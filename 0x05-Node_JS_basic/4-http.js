e2c511351fe3d4252d38fe958f6ae3bf4f906a97
dannykay62 <danielkay62@gmail.com>
2023-11-25 00:41:26 +0100

    Modified code for error

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,9 +3,9 @@ const http = require('http');
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.end('Hello Holberton School!');
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 app.listen(port);
 module.exports = app;
