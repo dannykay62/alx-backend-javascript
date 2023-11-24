3932ac4e9f55da13e3c5d672ac9738321d0968ab
dannykay62 <danielkay62@gmail.com>
2023-11-24 23:37:22 +0100

    Use Spies wrapper to execute the wrapped function and log useful information

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -8,7 +8,7 @@ const sendPaymentRequestToApi = require('./3-payment.js');
 describe('sendPaymentRequestToApi function', () => {
   const spyUtils = sinon.spy(Utils, 'calculateNumber');
 
-  it('this validates the use of Utils function', () => {
+  it('validate the usage of the Utils function', () => {
     sendPaymentRequestToApi(100, 20);
     chai.expect(spyUtils.calledOnce).to.be.true;
     chai.expect(spyUtils.calledWith('SUM', 100, 20)).to.be.true;
