0dd9146eadf098c7da8ecba6447d158d44f69619
dannykay62 <danielkay62@gmail.com>
2023-11-24 20:05:34 +0100

    Combining descriptions for test, create a new file with test cases

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,35 +3,36 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber type == SUM', () => {
-    it('this checks the output of the function', () => {
-        assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
-        assert.strictEqual(calculateNumber('SUM', 1, 37), 5);
-        assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-        assert.strictEqual(calculateNumber('SUM', 3.7, 1), 5);
-        assert.strictEqual(calculateNumber('SUM', 0.0, 0), 0);
-        assert.strictEqual(calculateNumber('SUM', -1, -1), -2);
-        assert.strictEqual(calculateNumber('SUM', -1, 1), 0);
-        assert.strictEqual(calculateNumber('SUM', 1, -1), 0);
-    });
+  it('this check the output of the function with different parameters', () => {
+    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+    assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+    assert.strictEqual(calculateNumber('SUM', 3.7, 1), 5);
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    assert.strictEqual(calculateNumber('SUM', 4.5, 1.4), 6);
+    assert.strictEqual(calculateNumber('SUM', 0.0, 0), 0);
+    assert.strictEqual(calculateNumber('SUM', -1, 1), 0);
+    assert.strictEqual(calculateNumber('SUM', 1, -1), 0);
+    assert.strictEqual(calculateNumber('SUM', -1, -1), -2);
+  });
 });
 
-describe('calculateNumber type == SUBSTRACT', () => {
-    it('this checks the output of the function', () => {
-        assert.strictEqual(calculateNumber('SUBSTRACT', -1, 1), -2);
-        assert.strictEqual(calculateNumber('SUBSTRACT', 2, 4.5), 3);
-        assert.strictEqual(calculateNumber('SUBSTRACT', 0.0, 5), -5);
-        assert.strictEqual(calculateNumber('SUBSTRACT', 5, 3), 2);
-        assert.strictEqual(calculateNumber('SUBSTRACT', 4.5, 2), 3);
-        assert.strictEqual(calculateNumber('SUBSTRACT', 3.1, 2.5), 0);
-        assert.strictEqual(calculateNumber('SUBSTRACT', -1.5, 0), -1);
-    });
+describe('calculateNumber type == SUBTRACT', () => {
+  it('this check the output of the function with different parameters', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 5, 3), 2);
+    assert.strictEqual(calculateNumber('SUBTRACT', 3.1, 2.5), 0);
+    assert.strictEqual(calculateNumber('SUBTRACT', 4.5, 2), 3);
+    assert.strictEqual(calculateNumber('SUBTRACT', 0.0, 5), -5);
+    assert.strictEqual(calculateNumber('SUBTRACT', 2, 4.5), -3);
+    assert.strictEqual(calculateNumber('SUBTRACT', -1, 1), -2);
+    assert.strictEqual(calculateNumber('SUBTRACT', -1.5, 0), -1);
+  });
 });
 
 describe('calculateNumber type == DIVIDE', () => {
-    it('checks for output of the function', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 1, 0), 'Error');
-        assert.strictEqual(calculateNumber('DIVIDE', 0.0, 2), 0);
-        assert.strictEqual(calculateNumber('DIVIDE', -1, 1), -1);
-        assert.strictEqual(calculateNumber('DIVIDE', 2, 2.5), 0.6666666666666666);
-    });
+  it('this check the output of the function with different parameters', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 2, 2.5), 0.6666666666666666);
+    assert.strictEqual(calculateNumber('DIVIDE', 0.0, 2), 0);
+    assert.strictEqual(calculateNumber('DIVIDE', -1, 1), -1);
+    assert.strictEqual(calculateNumber('DIVIDE', 1, 0), 'Error');
+  });
 });
