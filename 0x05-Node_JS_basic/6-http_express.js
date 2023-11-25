9673e0bc7be7a217ea5c48294697458b4122e5e9
dannykay62 <danielkay62@gmail.com>
2023-11-25 01:10:34 +0100

    Modified code for error

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello Holberton School!'))
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
 app.listen(1245);
 module.exports = app;
