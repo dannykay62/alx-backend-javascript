a355f6d1dbe10bfe90cdb68b3c81787432ca98f9
dannykay62 <danielkay62@gmail.com>
2023-11-25 01:09:01 +0100

    Modified code for error

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,20 +3,18 @@ const fs = require('fs');
 
 const port = 1245;
 const app = express();
-
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
   const databasePath = req.app.get('databasePath');
-
   fs.readFile(databasePath, 'utf8', (err, data) => {
     if (err) {
       res.status(500).send(`Error reading database: ${err.message}`);
     } else {
       const lines = data.split('\n').filter(line => line.trim() !== '');
-      const studentsList = lines.map(line => line.split(',')[0]);
+      const studentsList = lines.map(line => line.split(','[0]));
       res.send(`This is the list of our students:\n${studentsList.join('\n')}`);
     }
   });
@@ -31,8 +29,5 @@ if (!databaseName) {
 
 app.set('databasePath', databaseName);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
+app.listen(port);
 module.exports = app;
